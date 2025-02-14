0e50d33d71677aa84f5c5a6b48ebfd6c39ec1c28
dodoyoo <dong15903@daum.net>
2025-02-14 17:54:29 +0900

    "dev setting"

---
+++ src/main/java/com/springboot/shop/BasicController.java
@@ -0,0 +1,20 @@
+package com.springboot.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String hello() {
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String bye() {
+        return "잘가요";
+    }
+
+}
